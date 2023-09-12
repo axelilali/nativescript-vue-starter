99c0b3e4baf7962ff1d3b677808af317dd9a6f77
Axel Ilali <axel.ilali@yahoo.fr>
2023-09-12 14:27:06 +0100

    feat: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,14 @@ module.exports = {
 	extends: [
 		// add more generic rulesets here, such as:
 		// 'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
 		'plugin:vue/vue3-recommended',
 		// 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
 	],
+	parser: 'vue-eslint-parser',
+	parserOptions: { parser: '@typescript-eslint/parser' },
+	plugins: ['@typescript-eslint'],
+	root: true,
 	rules: {
 		'vue/max-attributes-per-line': [
 			'error',
